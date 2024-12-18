3fb96ac98ce5c810fa9a8268cf8f88e4e1b65b3b
Ankit Sonal Prasad <66120080+antimatter007@users.noreply.github.com>
2024-12-18 16:04:09 +0530

    css fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,8 @@
 // frontend/pages/_app.js
-
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import '../styles/globals.css'; // Import the global CSS
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <ToastContainer position="top-right" autoClose={5000} hideProgressBar={false} newestOnTop closeOnClick pauseOnFocusLoss draggable pauseOnHover />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
